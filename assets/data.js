04dabc0acd8be1001ecb23be1a1e02fa7d6f2236
Bima Novianto <124200003@student.upnyk.ac.id>
2024-06-24 11:10:02 +0700

    first commit

---
+++ assets/data.js
@@ -0,0 +1,171 @@
+var initialCardData = [
+    {
+        title: "Wuling.id",
+        url: "https://wuling.id/",
+        description: "Wuling Indonesia Website",
+        status: "Other",
+        image: "/assets/img/product/wulingid.png"
+    },
+    {
+        title: "Baidu Translator",
+        url: "https://fanyi.baidu.com/",
+        description: "Baidu Translator (China)",
+        status: "Other",
+        image: "/assets/img/product/baidu.png"
+    },
+    {
+        title: "CARD",
+        url: "https://card.wuling.co.id/",
+        description: "Claim Analysis Report Diagnose - Apps",
+        status: "Manufacture",
+        image: "/assets/img/product/card.png"
+    },
+    {
+        title: "CMMS",
+        url: "https://cmms.wuling.co.id/",
+        description: "Computerized Maintenance Management System",
+        status: "Manufacture",
+        image: "/assets/img/product/cmms.png"
+    },
+    {
+        title: "Drive Synology",
+        url: "https://drive.wuling.co.id:5001/",
+        description: "Cloud Storage SGMW",
+        status: "IT Support",
+        image: "/assets/img/product/drive.png"
+    },
+    {
+        title: "DCS",
+        url: "https://dms.wuling.co.id/DCS/UserManager/login.do",
+        description: "Dealer Collaboration System",
+        status: "Sales",
+        image: "/assets/img/product/dcs.png"
+    },
+    {
+        title: "DMS",
+        url: "https://dms.wuling.co.id/dms.web/html/index.html",
+        description: "Dealer Management System",
+        status: "Sales",
+        image: "/assets/img/product/dms.png"
+    },
+    {
+        title: "ECM (EWO/PAA)",
+        url: "http://ecm.sgmw.com.cn/innovatorserver/Client",
+        description: "EWO / PAA Apps",
+        status: "HeadQuarter",
+        image: "/assets/img/product/ewo.png"
+    },
+    {
+        title: "ECU",
+        url: "http://10.140.16.6/monitorHome",
+        description: "ECU Online Config Application Monitoring",
+        status: "HeadQuarter",
+        image: "/assets/img/product/ecu.png"
+    },
+    {
+        title: "HSSE",
+        url: "https://portal.wuling.co.id/",
+        description: "HSSE Portal Web",
+        status: "Other",
+        image: "/assets/img/product/hsse.png"
+    },
+    {
+        title: "Installer",
+        url: "http://installer.wuling.co.id/",
+        description: "List Installer / Application",
+        status: "IT Support",
+        image: "/assets/img/product/installer.png"
+    },
+    {
+        title: "LES",
+        url: "https://les.wuling.co.id/les_root/",
+        description: "Logistic Execution System",
+        status: "Manufacture",
+        image: "/assets/img/product/LES.png"
+    },
+    {
+        title: "LES Portal",
+        url: "https://srm.wuling.co.id/portal/",
+        description: "Logistic Portal Supplier",
+        status: "Manufacture",
+        image: "/assets/img/product/lesportal.png"
+    },
+    {
+        title: "Login Internet Access",
+        url: "http://login.wuling.ad/",
+        description: "Login for Access Internet in SGMW Network",
+        status: "IT Support",
+        image: "/assets/img/product/login.png"
+    },
+    {
+        title: "Meeting Room Booking",
+        url: "http://meeting.wuling.co.id/",
+        description: "Meeting Room Booking Apps",
+        status: "Other",
+        image: "/assets/img/product/meet.png"
+    },
+    {
+        title: "MES",
+        url: "https://mes.wuling.co.id/",
+        description: "Manufacturing Execution System",
+        status: "Manufacture",
+        image: "/assets/img/product/mes.png"
+    },
+    {
+        title: "NEWO (EPR)",
+        url: "https://idnoa.sgmw.com.cn/",
+        description: "NEWO / EPR Web",
+        status: "HeadQuarter",
+        image: "/assets/img/product/newo.png"
+    },
+    {
+        title: "NEWS Portal",
+        url: "http://news.wuling.co.id/news",
+        description: "Wuling Portal News",
+        status: "Other",
+        image: "/assets/img/product/newsportal.png"
+    },
+    {
+        title: "OCS Reports",
+        url: "http://ocs.wuling.co.id/",
+        description: "IT Asset Management Apps",
+        status: "IT Support",
+        image: "/assets/img/product/ocs.png"
+    },
+    {
+        title: "SGMW China",
+        url: "https://www.sgmw.com.cn/en/",
+        description: "SGMW HQ Website",
+        status: "HeadQuarter",
+        image: "/assets/img/product/sgmwhq.png"
+    },
+    {
+        title: "SGMW Multifinance",
+        url: "https://sgmwmultifinance.id/",
+        description: "SGMW Multifinance Website",
+        status: "Other",
+        image: "/assets/img/product/multifinance.png"
+    },
+    {
+        title: "Webmail SGMW",
+        url: "https://en.exmail.qq.com/login",
+        description: "Tencent Webmail",
+        status: "Other",
+        image: "/assets/img/product/webmail.png"
+    },
+    {
+        title: "WMS Spareparts",
+        url: "https://wmssp.wuling.co.id/",
+        description: "Warehouse Management System - After Sales Spareparts",
+        status: "After Sales",
+        image: "/assets/img/product/wmssp.png"
+    },
+    {
+        title: "WMS Vehicle",
+        url: "https://wmsv.wuling.co.id/",
+        description: "Warehouse Management System - Sales Vehicles",
+        status: "Sales",
+        image: "/assets/img/product/wmsv.png"
+    },
+    
+];
